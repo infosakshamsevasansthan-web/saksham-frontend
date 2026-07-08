a7cab8d7899c2bda13b1c80da331025e9c12e7df
infosakshamsevasansthan-web <infosakshamsevasansthan@gmail.com>
2026-07-08 16:33:37 +0530

    Update import path for SuccessModal component

--- src/pages/Admin/HR/WorkAssignment.jsx
+++ src/pages/Admin/HR/WorkAssignment.jsx
@@ -8,7 +8,7 @@ import axios from 'axios';
 import { toast, Toaster } from 'react-hot-toast';
 
 // --- Imports ---
-import SuccessModal from './components/SuccessModal';
+import SuccessModal from '../../../components/SuccessModal';
 import CollectorForm from './forms/CollectorForm';
 import RoadSweeperForm from './forms/RoadSweeperForm';
 import InspectorForm from './forms/InspectorForm';
